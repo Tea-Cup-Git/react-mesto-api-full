853213196a31580d2865a69af1d509355f227a22
Tatiana Lebedeva <tea-cup-mail@yandex.ru>
2021-01-30 21:44:47 +0300

    test: правки для подключения к api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -98,7 +98,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-13',
+  baseUrl: 'http://lebedeva.students.nomoredomains.work',
   token: '05e19680-152e-4468-99ab-dd133195a5d0'
 })
 
