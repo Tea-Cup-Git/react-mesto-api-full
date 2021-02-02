f334a1f26296e01079ff8f5928f85e5f53d25820
Tatiana Lebedeva <tea-cup-mail@yandex.ru>
2021-02-02 07:36:56 +0300

    hotfix: мелкие правки

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -99,7 +99,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'api.lebedeva.students.nomoredomains.work',
+  baseUrl: 'http://api.lebedeva.students.nomoredomains.work',
   headers: {
     'Accept': 'application/json',
     'Content-type': 'application/json'
